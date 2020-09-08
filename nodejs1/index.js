23882eb409af96a1e6e6cead294559ce2d1dcd2b
thuanan <anharry14@gmail.com>
2020-09-08 15:16:17 +0700

    get start with pug

--- nodejs1/index.js
+++ nodejs1/index.js
@@ -1,10 +1,23 @@
- const express = require("express");
- const app = express();
+const express = require("express");
+const app = express();
 
 const port = 3001;
 
+
+app.set('view engine', 'pug')
+app.set('views', './views')
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.render('index');
+})
+
+app.get('/user',(req ,res) =>{
+	res.render('./user/user',
+		{
+			users: [{id: 1, name: 'Thuan An'},
+					{id: 2, name: 'Hung Si'},
+					{id: 3, name: 'Thai Bao'}]
+		}
+		)
 })
 
 app.listen(port, () => {console.log('Example app listening at http://localhost:' + port)});
