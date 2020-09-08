3f8f97d48343a129acfbf98d1537e0607d4633b3
thuanan <anharry14@gmail.com>
2020-09-08 21:42:15 +0700

    learn Post method

--- nodejs1/index.js
+++ nodejs1/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const app = express();
+
 
 const port = 3001;
 
-var users =[{id: 1, name: 'Thuan An'},
+var users = [{id: 1, name: 'Thuan An'},
 			{id: 2, name: 'Hung Si'},
 			{id: 3, name: 'Thai Bao'}]
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.set('view engine', 'pug')
 app.set('views', './views')
 app.get('/', (req, res) => {
@@ -14,20 +20,22 @@ app.get('/', (req, res) => {
 
 app.get('/user',(req ,res) =>{
 	res.render('./user/user',
-		{
-			users: users
-		}
-		)
+		{ users: users })
 })
 
-app.get('/user/search', (req, res)=>{
+app.get('/user/search', (req, res) => {
 	var search = req.query.q;
 	var userSearch = users.filter(function(user){
-		return user.name.toLowerCase().indexOf(search.toLowerCase()) !==-1
-	})
+		return user.name.toLowerCase().indexOf(search.toLowerCase()) !==-1 })
 	res.render('./user/user', {users: userSearch, search: req.query.q })
-}
+})
 
-)
+app.get('/user/create', (req, res) =>{
+	res.render('./user/creat_user')
+})
 
+app.post('/user/create', (req,res) =>{
+	users.push(req.body)
+	res.redirect('/user')
+})
 app.listen(port, () => {console.log('Example app listening at http://localhost:' + port)});
